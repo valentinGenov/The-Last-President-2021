bca27dfbf08f42e2852d9db4cb40375ceb1d11cb
Damian <65552465+damian568@users.noreply.github.com>
2021-01-03 18:54:50 +0200

    Add files via upload

---
+++ The Last President/The Last President/Character.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Last_President
+{
+    class Character
+    {
+        private string name;
+        private string sex;
+        private int age;
+        private string placeOfBirth;
+        private string occupation;
+
+        public Character(string n, string s, int a)
+        {
+            this.Name = n;
+            this.Sex = s;
+            this.Age = a;
+            this.placeOfBirth = "Japan";
+            this.occupation = "Soldier(BodyGuard)";
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Sex
+        {
+            get { return this.sex; }
+            set { this.sex = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if( value > 0 && value < 90)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    Console.WriteLine("This person is very old! ");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + this.Name + " " + " Sex: " + this.Sex + " Age: " + this.Age + " placeOfBirth: " + this.placeOfBirth + " Occupation " + this.occupation;
+        }
+    }
+}
---
+++ The Last President/The Last President/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace The_Last_President
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter the Sex (Male or Female): ");
+            string sex = Console.ReadLine();
+
+            Console.WriteLine("Enter the Age: ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            Character firstCharacter = new Character(name, sex, age);
+            Console.WriteLine(firstCharacter);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if (sex == "male" || sex == "Male")
+            {
+                Console.WriteLine("The two presidents gathered to talk about their military power. At one time a president falls to the ground with a high fever and difficulty breathing and snuggle bouquet.Immediately evacuated another president and start Studies have body president. It soon became clear that he had been infected with the Covid-19.");
+                Console.WriteLine("The president decides to leave his country.He wonders who will choose to be with him as a bodyguard and decides to choose you.");
+                Console.WriteLine("The President: If someone has to be my bodyguard, I prefer it to be you.");
+            }
+            else
+            {
+                Console.WriteLine("The two presidents gathered to talk about their military power. At one time a president falls to the ground with a high fever and difficulty breathing and snuggle bouquet.Immediately evacuated another president and start Studies have body president. It soon became clear that he had been infected with the Covid-19.");
+                Console.WriteLine("The president decides to leave his country.He wonders who will choose to be with him as a bodyguard and decides to choose you.");
+                Console.WriteLine("The President: If someone has to be my bodyguard, I prefer it to be you.");
+            }
+        }
+    }
+}
